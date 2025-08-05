46b12efe17caf3de9998f64dc4514a6b3f5071aa
Owner <ghifariezraramadhan@gmail.com>
2025-08-05 14:12:19 +0700

    FIX : LOGOUT

--- src/components/AuthContext.tsx
+++ src/components/AuthContext.tsx
@@ -5,29 +5,14 @@ import { FetchUser, Logout } from '@/services/auth';
 
 export function AuthProvider({ children }: React.PropsWithChildren) {
 	const [user, setUser] = useState<Payload | null>(null);
-	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
-		const fetchData = async () => {
-			await FetchUser({ setUser });
-			setLoading(false);
-		};
-		fetchData();
+		FetchUser({ setUser });
 	}, []);
 
 	const logout = useCallback(async () => {
-		try {
-			await Logout({ setUser });
-			setUser(null);
-		} catch (err) {
-			console.error('Logout error:', err);
-		}
+		await Logout({ setUser });
 	}, []);
-
-	if (loading) {
-		// bisa diganti skeleton / spinner kalau mau
-		return <div>Loading...</div>;
-	}
 
 	return <AuthContext.Provider value={{ user, setUser, logout }}>{children}</AuthContext.Provider>;
 }
